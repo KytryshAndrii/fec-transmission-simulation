11352f4bec5ffc5d4ee23b4305fbdffb6239f20a
Piotr Skrzyński <76049506+piotrskrzynski1@users.noreply.github.com>
2024-11-03 21:47:55 +0100

    Changed GilbertElliot.py to make it work with both Hamming and Convolutional Codes

--- Utils/GilbertElliot.py
+++ Utils/GilbertElliot.py
@@ -7,7 +7,7 @@ class GilbertElliottChannel:
         self.p_err_bad = p_err_bad    # BER w stanie złym (szansa od 0.0 do 1.0)
         self.state = 0  # 0 - dobry, 1 - zły
 
-    def transmit(self, bitsarray):
+    def transmitHamming(self, bitsarray):
         receivedArray = []
         errorsArray = []
         for array in bitsarray:
@@ -29,6 +29,38 @@ class GilbertElliottChannel:
                 errors.append(error)
                 received_bit = (bit + int(error)) % 2
                 received.append(received_bit)
+            receivedArray.append(received)
+            errorstemp = []
+            for error in errors:
+                if (error==True):
+                    errorstemp.append(1)
+                else:
+                    errorstemp.append(0)
+
+            errorsArray.append(errorstemp)
+        return receivedArray, errorsArray
+    def transmitConvolutional(self, bitsarray):
+        receivedArray = []
+        errorsArray = []
+        for bit in bitsarray:
+            received = []
+            errors = []
+
+            # Aktualizacja stanu
+            if self.state == 0:
+                if np.random.rand() < self.chance_for_bad:
+                    self.state = 1
+            else:
+                if np.random.rand() < self.chance_for_good:
+                    self.state = 0
+            # Błąd w zależności od stanu
+            if self.state == 0:
+                error = np.random.rand() < self.p_err_good
+            else:
+                error = np.random.rand() < self.p_err_bad
+            errors.append(error)
+            received_bit = (bit + int(error)) % 2
+            received.append(received_bit)
             receivedArray.append(received)
             errorstemp = []
             for error in errors:
